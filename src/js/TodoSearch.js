1661a821958d7ef0afd0192c046dd3e5fc57ff6f
Elver Rodriguez <adelson960@gmail.com>
2021-09-20 11:50:27 -0500

    Se usa el state de react

--- src/js/TodoSearch.js
+++ src/js/TodoSearch.js
@@ -2,16 +2,20 @@ import React from 'react';
 import '../css/TodoSearch.css';
 
 function TodoSearch() {
+  const [searcValue, setSearchValue] = React.useState('');
+
   const onSearch = (evt) => {
-    console.log('Buscando', evt.target.value)
+    setSearchValue(evt.target.value)
   }
-  return (
+  return ([
     <input
       className="TodoSearch"
       placeholder="Programar login"
+      value={searcValue}
       onChange={onSearch}
-    />
-  );
+    />,
+    <p>{searcValue}</p>
+  ]);
 }
 
 export { TodoSearch };
